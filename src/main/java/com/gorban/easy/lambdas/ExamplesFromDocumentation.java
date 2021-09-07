216dc5acf5d36071667ba92e983ab6ee2f4e961d
Dmytro Horban <dmytro.horban@tideworks.com>
2021-09-07 14:52:49 +0300

    Added new examples.

--- src/main/java/com/gorban/easy/lambdas/ExamplesFromDocumentation.java
+++ src/main/java/com/gorban/easy/lambdas/ExamplesFromDocumentation.java
@@ -8,36 +8,6 @@ import java.util.stream.Collectors;
 
 public class ExamplesFromDocumentation {
 
-    static class Person {
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public Person(String name) {
-            this.name = name;
-        }
-    }
-
-    static class Employee {
-        private String name;
-        private int salary;
-
-        public Employee(String name, int salary) {
-            this.name = name;
-            this.salary = salary;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getSalary() {
-            return salary;
-        }
-    }
-
     public static void main(String[] args) {
         final var persons = new ArrayList<Person>();
         persons.add(new Person("Ivanov"));
@@ -54,12 +24,120 @@ public class ExamplesFromDocumentation {
         System.out.println("Joined people: " + joined);
 
         final var employees = List.of(new Employee("Ivanov", 100),
-                new Employee("Petrov", 200),
-                new Employee("Sidorov", 300));
+                                      new Employee("Petrov", 200),
+                                      new Employee("Sidorov", 300));
 
         final var employeesSalary = employees.stream()
-                .collect(Collectors.summarizingInt(Employee::getSalary));
+                                             .collect(Collectors.summarizingInt(Employee::getSalary));
 
         System.out.println("employeesSalary: " + employeesSalary);
+
+        // Group Employees by department:
+        final var employeesDepartments = List.of(
+                new Employee("Ivanov", 100, "Security"),
+                new Employee("Petrov", 200, "IT"),
+                new Employee("Sidorov", 300, "IT"));
+
+        final var employeesByDepartment = employeesDepartments.stream()
+                                                              .collect(Collectors.groupingBy(Employee::getDepartment));
+        System.out.println("employeesByDepartment: " + employeesByDepartment);
+
+        // Compute sum of salaries by department
+        final var salariesByDepartment = employeesDepartments.stream()
+                                                             .collect(Collectors.groupingBy(Employee::getDepartment,
+                                                                                            Collectors.summarizingInt(
+                                                                                                    Employee::getSalary)));
+        System.out.println("salariesByDepartment: " + salariesByDepartment);
+
+        // Partition students into passing and failing
+        final var students = List.of(
+                new Student("Ivanov", 3),
+                new Student("Petrov", 4),
+                new Student("Sidorov", 5));
+
+        final var passingFailing =
+                students.stream().collect(Collectors.partitioningBy(s -> s.getGrade() >= Student.PASS_THRESHOLD));
+
+        System.out.println("passingFailing: " + passingFailing);
+    }
+
+    static class Student {
+
+        private static int PASS_THRESHOLD = 4;
+        private int grade;
+        private String lastName;
+
+        public Student(final String lastName, final int grade) {
+            this.grade = grade;
+            this.lastName = lastName;
+        }
+
+        public int getGrade() {
+            return grade;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "grade=" + grade +
+                    ", lastName='" + lastName + '\'' +
+                    '}';
+        }
+    }
+
+    static class Person {
+
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    static class Employee {
+
+        private String name;
+        private int salary;
+        private String department;
+
+        public Employee(String name, int salary, String department) {
+            this.name = name;
+            this.salary = salary;
+            this.department = department;
+        }
+
+        public Employee(String name, int salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "name='" + name + '\'' +
+                    ", salary=" + salary +
+                    ", department='" + department + '\'' +
+                    '}';
+        }
     }
 }
